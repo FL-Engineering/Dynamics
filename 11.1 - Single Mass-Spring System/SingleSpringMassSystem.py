f509683bc1bb4357a7590d0a3cac3bd35aa68388
FL-Engineering <92333523+FL-Engineering@users.noreply.github.com>
2022-01-29 15:32:23 -0500

    Create SingleSpringMassSystem.py

---
+++ 11.1 - Single Mass-Spring System/SingleSpringMassSystem.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################################################################
+####                         Define Parameters                             ####
+
+g = 9.81    # [m/s^2]
+m = 0.185   # [kg]
+l = 1.22    # [m]
+
+duration = 5
+dt = 0.01
+
+theta_initial = 90      # [deg]
+thetaDot_initial = 0    # [deg/s]
+
+###############################################################################
