284e8975dc320809157ccfc75fdcd0674066a31d
FL-Engineering <92333523+FL-Engineering@users.noreply.github.com>
2022-01-29 15:55:25 -0500

    Update SingleSpringMassSystem.py
    Added comments to code

--- 11.1 - Single Mass-Spring System/SingleSpringMassSystem.py
+++ 11.1 - Single Mass-Spring System/SingleSpringMassSystem.py
@@ -11,36 +11,36 @@ m = 5                           # Mass [kg]
 k = 50                          # Spring Constant [N/m]
 UnstrechedSpringLength = 0.5    # [m]
 
-duration = 5
-dt = 0.01
+duration = 5        # How long the simulation will run for [sec]
+dt = 0.01           # Time step of the simulation [sec]
 
-x_initial = 0.5     # [m]
-xDot_initial = 0   # [m/s]
+x_initial = 0.5     # Initial displacement of the spring [m]
+xDot_initial = 0    # Initial velocity of the mass [m/s]    
 
 ###############################################################################
 ####                             Functions                                 ####
 
 def model(G,t):
-    x = G[0]
-    Q = G[1]
+    x = G[0] # Displacement of mass for this time step [m]
+    Q = G[1] # Velocity of the mass for this time step [m/s]
     
-    QDot = -k*x/m
+    QDot = -k*x/m   # Differential Equation of Motion
     
     return [Q, QDot]
 
 ###############################################################################
 ####                      Solving Differential Equation                    ####
 
-z0 = [x_initial, xDot_initial]
+z0 = [x_initial, xDot_initial]          # Initial conditions
 
-time = np.arange(0,duration,dt)
+time = np.arange(0,duration,dt)         # Time array
 
 # Creating vectors to save data
-ParticlePosition = np.zeros_like(time)
+ParticlePosition = np.zeros_like(time)    
 ParticleVelocity = np.zeros_like(time)
 
-# Saving initial coniditions
-ParticlePosition[0] = z0[0] + UnstrechedSpringLength
+# Saving first iteration of initial coniditions
+ParticlePosition[0] = z0[0] + UnstrechedSpringLength    # Actual position of mass is the unstretched spring length added to the displacement, x
 ParticleVelocity[0] = z0[1]
 
 for ii in range(1,len(time)):
@@ -48,6 +48,7 @@ for ii in range(1,len(time)):
     tspan = [time[ii-1], time[ii]]
     z = odeint(model, z0, tspan)
     
+    # Saving solution at each time step
     ParticlePosition[ii] = z[1][0] + UnstrechedSpringLength
     ParticleVelocity[ii] = z[1][1]
     
